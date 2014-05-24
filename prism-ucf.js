6ad751ff2b4bb893d09a49188ff0ff469a7f3571
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2014-05-24 22:50:26 +0200

    Add basic highlighting for .ucf (user constraints file)

---
+++ prism-ucf.js
@@ -0,0 +1,15 @@
+Prism.languages.ucf = {
+  'keyword': /\b(TNM_NET|NET|LOC|PERIOD|IOSTANDARD|TIMESPEC|CONFIG|DRIVE|SLEW)\b/g,
+  'comment': /#[^\r\n]*(\r?\n|$)/g,
+  'string': /("|')(\\?.)*?\1/g,
+};
+
+Prism.languages.insertBefore('ucf', 'keyword', {
+  'important': {
+    pattern: /\s+(\||\<|\>)/g,
+    inside: {
+      'important': /(\|)/,
+    },
+    rest: Prism.languages.ucf
+  }
+});
